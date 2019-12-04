f1e3a1224eb7ccdc1cdcbca29b6759cd0ab045ee
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-04 11:16:12 +0100

    génération des models Patient et Practionner avec devise

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,9 @@
 class Patient < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   has_many :appointments, dependent: :delete_all #erreur de violation de foreign key, cette ligne permet de créer le seed sans encombre 
   has_many :practionners, through: :appointments
 
