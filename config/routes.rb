587b545b756918de3139fecbdf6b81a615a62d69
Antoinebousquet <moodlinkproject@gmail.com>
2019-12-04 20:28:22 +0100

    [Done] back routes resources for patients done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :patients
   devise_for :practionners
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :patients
  
 end
