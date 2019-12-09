16ca83af6a05c3de9736cd35499b4e2b083d9623
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-09 18:46:33 +0100

    [DONE] BACK change seed puts

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,33 +17,33 @@ DataShow.destroy_all
     email: "email#{practionner}@example.com", password: "Password#{practionner}", 
     sexe: "Sexe#{practionner}", specialty:"Spécialité#{practionner}", adress: "Adresse#{practionner}", 
     diploma: "Diplômes#{practionner}", certifications: "Autres certifications#{practionner}")
-  puts "20 praticiens ont été crées"
 end
+puts "20 praticiens ont été crées"
 
 20.times do |patient|
   Patient.create(first_name: "Prénom#{patient}",last_name: "Nom#{patient}", 
     email: "email#{patient}@example.com", password: "Password#{patient}", 
-    treatment: "Traitement#{patient}", ville: "Ville#{patient}", sexe: "Sexe#{patient}",
+    treatment: "Traitement#{patient}", city: "Ville#{patient}", sexe: "Sexe#{patient}",
     birth: DateTime.strptime("09/14/1992 10:00", "%m/%d/%Y %H:%M"))
-  puts "20 patients ont été crées"
 end
+puts "20 patients ont été crées"
 
 100.times do |appointment|
   Appointment.create(date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"),lieu: "Lieu#{appointment}", 
     patient_id: Patient.all.sample.id, practionner_id: Practionner.all.sample.id )
-  puts "100 rendez-vous ont été crées"
 end
+puts "100 rendez-vous ont été crées"
 
 10.times do |day|
   Day.create(date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), hours_of_sleep: "Heures de sommeil#{day}",
     mood: "Humeur#{day}", treatment: "Traitement#{day}")
-  puts "10 jours ont été crées"
 end
+puts "10 jours ont été crées"
 
 100.times do |datashow|
   DataShow.create(patient_id: Patient.all.sample.id, day_id: Day.all.sample.id )
-  puts "100 datashow ont été crées"
 end
+puts "100 datashow ont été crées"
 
 puts "le seed est fait !"
 
