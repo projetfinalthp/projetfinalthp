2fb055a8585d0f6c1f3c61de75da4b8f4e30a442
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 11:14:05 +0100

    [DOING] BACK validates patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -10,7 +10,17 @@ class Patient < ApplicationRecord
   has_many :data_shows, dependent: :delete_all
   has_many :days, through: :data_shows
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :birth, 
+
+  validate :validate_age
+
+
+  def validate_age
+      if birth.present? && birth.to_datetime > 18.years.ago.to_datetime
+          errors.add(:birth, 'Vous devez être majeur')
+      end
+  end
 
 end
