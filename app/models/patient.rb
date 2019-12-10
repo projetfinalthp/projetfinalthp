9db18a10a907445257c8bf1c5c12179b636e1dab
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 12:39:45 +0100

    [DONE] BACK validates patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -12,10 +12,6 @@ class Patient < ApplicationRecord
 
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
-  validates :treatment, length: {maximum: 500}
-
-
-
- 
+  validates :treatment, length: { maximum: 500 }
 
 end
