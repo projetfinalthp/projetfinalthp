72fca5362b3ff8cdcc92745e11e36364b56d723f
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 19:56:13 +0100

    [DONE] BACK controller avatar patient

--- app/controllers/avatars_patient_controller.rb
+++ app/controllers/avatars_patient_controller.rb
@@ -1,7 +1,8 @@
 class AvatarsPatientController < ApplicationController
+
 	def create
     @patient = Patient.find(params[:patient_id])
     @patient.avatar.attach(params[:avatar])
     redirect_to(patient_path(@patient))
-  end
+ 	end
 end
