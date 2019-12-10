113ca2241b26672f27eb491d0d2df184cb5acec8
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 13:15:21 +0100

    [DONE] validates table day

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,9 @@
 class Day < ApplicationRecord
   has_many :data_shows, dependent: :delete_all #This line enable to create a seed without problem
   has_many :patients, through: :data_shows
+
+  validates :date, presence: true
+  validates :hours_of_sleep, presence: true, length: {maximum: 2}
+  validates :mood, presence: true
+  validates :treatment, presence: true
 end
