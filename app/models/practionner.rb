d9dd88328a7a3fbaf43e92441f13464a591c12e8
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 12:40:13 +0100

    [DONE] BACK validates practionner

--- app/models/practionner.rb
+++ app/models/practionner.rb
@@ -6,4 +6,9 @@ class Practionner < ApplicationRecord
          
   has_many :appointments, dependent: :delete_all #This line enable to create a seed without problem
   has_many :patients, through: :appointments
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :specialty, length: { maximum: 300 }
+  validates :adress, presence: true
 end
