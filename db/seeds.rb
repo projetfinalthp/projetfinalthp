9a8214b86c88c2661668268a95b4d5a8d206b171
Antoinebousquet <moodlinkproject@gmail.com>
2019-12-10 15:21:36 +0100

    small change in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
 # Practionner.destroy_all
 # Patient.destroy_all
 # Appointment.destroy_all
