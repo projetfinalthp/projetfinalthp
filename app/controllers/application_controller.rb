42845bd7e60c04d446c170272111eeb1e31388c7
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 14:46:42 +0100

    [DOING] BACK Register avatar with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
 	 	 end
      def configure_devise_parameters
        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
-       devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+       devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)}
      end
  end
