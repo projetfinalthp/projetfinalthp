9663e3ae6cb7f7aef1fb93f00ac13fbf61d05687
KPLUU <kph.luu@gmail.com>
2019-12-10 01:59:05 +0100

    [DOING] - Front MES-RDV add navbar + dropdown + avatar OK

--- app/controllers/doctor_appointment_list_controller.rb
+++ app/controllers/doctor_appointment_list_controller.rb
@@ -1,6 +1,6 @@
 class DoctorAppointmentListController < ApplicationController
   def show
   	@patient_practionners = Patient.find(params[:id]).practionners
-  	@patient_appointments = Patient.find(params[:id]).appointments
+    @patient_appointments = Patient.find(params[:id]).appointments
   end
 end
