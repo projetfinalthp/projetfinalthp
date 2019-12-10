af5a6182ac31b9c139c96e31443d3dd794a36f0f
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-10 16:36:17 +0100

    [DOING] BACK config routes avatar patient

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :patients
   devise_for :practionners
-  resources :patients 
+
+  resources :patients  do
+    resources :avatars, only: [:create]
+  end
+   
   resources :practionners
   resources :patient_list
   resources :doctor_appointment_list
