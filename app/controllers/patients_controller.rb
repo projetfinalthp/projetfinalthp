15323d5677d6b4e6100db2b2b026f59b2a2e9fa0
Antoinebousquet <moodlinkproject@gmail.com>
2019-12-11 16:03:20 +0100

    ok

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -16,7 +16,8 @@ before_action :authorize_user, except: [:index]
 
   def authorize_user 
     @user = Patient.find(params[:id])
-    unless current_patient == @user || current_practionner = @user.practionner
+    @user_practionner = @user.practionner.find(params[:id])
+    unless current_practionner = @user_practionner
       flash[:danger] = "Please log in."
       redirect_to new_patient_session_path
     end
