28803f52b94dfe0661e08c75a7996c64e288df69
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-11 11:45:31 +0100

    [DONE] BACK code to purge avatar Patient controller

--- app/controllers/avatars_patient_controller.rb
+++ app/controllers/avatars_patient_controller.rb
@@ -2,6 +2,7 @@ class AvatarsPatientController < ApplicationController
 
 	def create
     @patient = Patient.find(params[:patient_id])
+    @patient.avatar.purge
     @patient.avatar.attach(params[:avatar])
     redirect_to(patient_path(@patient))
  	end
