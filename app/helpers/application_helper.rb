d36d971cef8ec5361b30d1c0c3651c24df1b438d
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-11 14:36:23 +0100

    [TESTING] BACK trying to set avatar for patient or practionner in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,13 @@ module ApplicationHelper
 			gravatar_image_url(current_practionner.email)
 		end
 	end
+
+	def current_patient_avatar(current_patient)
+		if current_patient.avatar.attached?
+			current_patient.avatar
+		else
+			gravatar_image_url(current_patient.email)
+		end
+	end
+
 end
