5f775e82b9a826dccb982f152856020c6399f25b
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-11 12:36:09 +0100

    [DONE] BACK helper avatar practionner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
 		end
 	end
 
+	def current_practionner_avatar(current_practionner)
+		if current_practionner.avatar.attached?
+			current_practionner.avatar
+		else
+			gravatar_image_url(current_practionner.email)
+		end
+	end
 end
