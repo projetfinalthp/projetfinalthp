da4411291b029ab8bfb9876ed532c059b06f66e7
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-11 11:46:04 +0100

    [DONE] BACK code to purge avatar Practionner controller

--- app/controllers/avatars_practionner_controller.rb
+++ app/controllers/avatars_practionner_controller.rb
@@ -1,6 +1,7 @@
 class AvatarsPractionnerController < ApplicationController
 	def create
     @practionner = Practionner.find(params[:practionner_id])
+    @practionner.avatar.purge
     @practionner.avatar.attach(params[:avatar])
     redirect_to(practionners_path(@practionner))
   	end
