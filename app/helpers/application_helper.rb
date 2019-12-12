5182a736936b3ba0d4f39e1587ec7962785cf8e7
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-12 15:41:57 +0100

    [DONE] BACK suppress helper for avatar in devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
 
-	def resource_avatar
-		if resource.avatar.attached?
-			resource.avatar
-		else
-			gravatar_image_url(resource.email)
-		end
-	end
-
 	def current_practionner_avatar(current_practionner)
 		if current_practionner.avatar.attached?
 			current_practionner.avatar
