e4565fab0d4f2d4d20ec6dd09dd0d190481b060e
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-12 18:26:53 +0100

    [DONE] BACK suppress attribut avatar from devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
          
      def configure_devise_parameters
        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
-       devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)}
+       devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
      end
  end
