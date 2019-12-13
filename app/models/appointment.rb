dd77f11f5a1093452963b3287f54e9c31a499829
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-13 15:27:59 +0100

    [TESTING] BACK validates appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,11 @@ class Appointment < ApplicationRecord
   belongs_to :practionner
   belongs_to :patient
 
+  validates :date, presence: true
+  validates :place, presence: true 
+  validates :patient_id, presence: true 
+  validates :practionner_id, presence: true 
+
   after_create :patient_send_email, :practionner_send_email
 
   def patient_send_email
