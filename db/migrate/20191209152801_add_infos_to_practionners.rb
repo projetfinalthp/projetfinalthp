1bbf7c2354a62f287f29c085e934ba0b4f330442
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-13 15:26:31 +0100

    [DONE] BACK add phone to table practionners

--- db/migrate/20191209152801_add_infos_to_practionners.rb
+++ db/migrate/20191209152801_add_infos_to_practionners.rb
@@ -3,6 +3,7 @@ class AddInfosToPractionners < ActiveRecord::Migration[5.2]
   	add_column :practionners, :specialty, :text
     add_column :practionners, :sexe, :string
     add_column :practionners, :adress, :string
+    add_column :practionners, :phone, :integer
     add_column :practionners, :diploma, :text
     add_column :practionners, :certifications, :text
   end
