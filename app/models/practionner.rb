5e822f2e0134fa77ed0955517273c0db74c421dc
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-13 15:29:55 +0100

    [TESTING] BACK validates practionner

--- app/models/practionner.rb
+++ app/models/practionner.rb
@@ -8,6 +8,12 @@ class Practionner < ApplicationRecord
   has_many :appointments, dependent: :delete_all #This line enable to create a seed without problem
   has_many :patients, through: :appointments
 
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :specialty, length: { maximum: 300 }
+  validates :adress, presence: true
+  validates :phone, presence: true
+
   def welcome_send
     PractionnerMailer.welcome_email(self).deliver_now
   end
