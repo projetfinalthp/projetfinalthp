b1bda7094b6d7af4deb90a958a16f68cf38a9fc5
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-12-13 16:18:55 +0100

    [DONE] BACK validates patient, practionner, day, appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,9 @@ class Appointment < ApplicationRecord
   belongs_to :patient
 
   after_create :patient_send_email, :practionner_send_email
+  
+  validates :date, presence: true
+  validates :place, presence: true 
 
   def patient_send_email
   	AppointmentMailer.patient_email(self).deliver_now
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -11,6 +11,10 @@ class Patient < ApplicationRecord
   has_many :data_shows, dependent: :delete_all
   has_many :days, through: :data_shows
 
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :treatment, length: { maximum: 200 }
+
   def welcome_send
     PatientMailer.welcome_email(self).deliver_now
   end
